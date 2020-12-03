1318a93b90894c00bd947eee31ccd4151ee83a2a
mr-bl3r <j.griffioen@student.fontys.nl>
2020-12-03 20:58:36 +0100

    garbage delete

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,9 @@
 #include <Arduino.h>
-// is het nog steeds een probleem ?
-// test 10
+
 void setup() {
-  // This is a test
-  // another one bites the dust
+
 }
 
 void loop() {
-  // Dit ook
-  //verandering 2
-  // hello 
-  // sup
+
 }
