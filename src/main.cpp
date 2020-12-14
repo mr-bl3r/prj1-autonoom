21bafd189c7fcf07831b84d27c29c18b73d5e68d
mr-bl3r <j.griffioen@student.fontys.nl>
2020-12-14 18:08:30 +0100

    bochten rijden, etc

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,69 @@
 #include <Arduino.h>
-#include <Besturing.cpp>
+
+#define DS1 22
+#define DS2 24
+#define DS3 26
+#define DS4 28
+#define DS5 30
+#define DS6 32
+
+int delayR = 50;
+int bl = 1;
+int wh = 0;
+
+
+void straight() {
+  Serial.println("Driving straight");
+  //Driving forward
+  //Motor A forward @ full speed
+    digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+    digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+    analogWrite(3, 255);   //Spins the motor on Channel A at full speed
+
+  //Motor B forward @ full speed
+    digitalWrite(13, HIGH);  //Establishes forward direction of Channel B
+    digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+    analogWrite(11, 255);    //Spins the motor on Channel B at full speed
+
+}
+
+void right() {
+  Serial.println("Gaat naar rechts");
+  //Driving right
+  //Motor A forward @ full speed forward
+    digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+    digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+    analogWrite(3, 255);   //Spins the motor on Channel A at full speed
+
+  //Motor B forward @ full speed backward
+    digitalWrite(13, LOW);  //Establishes backward direction of Channel B
+    digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+    analogWrite(11, 255);    //Spins the motor on Channel B at full speed backward
+    
+}
+
+void left() {
+  Serial.println("Gaat naar links");
+  //Driving left
+  //Motor A forward @ full speed backward
+    digitalWrite(12, LOW); //Establishes backward direction of Channel A
+    digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+    analogWrite(3, 255);   //Spins the motor on Channel A at full speed backward
+
+  //Motor B forward @ full speed forward
+    digitalWrite(13, HIGH);  //Establishes forward direction of Channel B
+    digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+    analogWrite(11, 255);    //Spins the motor on Channel B at full speed forward
+    
+
+}
+
+void pause() {
+  Serial.println("start pauze"); 
+
+}
+
+
 
 void setup() {
   
@@ -10,13 +74,75 @@ void setup() {
   //Setup Channel B
   pinMode(13, OUTPUT); //Initiates Motor Channel A pin
   pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
+
+  //Setup sensor
+  pinMode(DS1, INPUT);
+  pinMode(DS2, INPUT);
+  pinMode(DS3, INPUT);
+  pinMode(DS4, INPUT);
+  pinMode(DS5, INPUT);
+  pinMode(DS6, INPUT);
   
   Serial.begin(9600); //Starts the serial monitor
 }
 
 void loop(){
+  // assigns signal 1-6 from digital sensor 1-6
+  int s1 = digitalRead(DS1);
+  int s2 = digitalRead(DS2);
+  int s3 = digitalRead(DS3);
+  int s4 = digitalRead(DS4);
+  int s5 = digitalRead(DS5);
+  int s6 = digitalRead(DS6);
   
-  recht();    //zie besturing.cpp voor uitleg
+  //Straight line driving
+  if ((s1 == bl) && (s2 == wh) && (s3 == wh) && (s4 == wh) && (s5 == wh)){
+        digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+        digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+        straight();
+        delay(delayR);
+       loop();
+  }
+
+  //Rechts slap
+  if ((s1 == bl) && (s6 == bl) && (s2 == wh) && (s3 == wh) && (s4 == wh)){
+    if (s5 == bl){
+      if (s6 == wh){
+        digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+        digitalWrite(8, HIGH);  //Engage the Brake for Channel B 
+        right();
+        delay(delayR);
+        loop();
+    }
+   }
+  }
+
+  //Rechts scherp
+  if ((s1 == bl) && (s5 == bl) && (s2 == wh) && (s4 == wh)){
+    if (s3 == bl){
+      if (s6 == wh){
+        digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+        digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+        right();
+        loop();
+      }
+    }
+  }
+
+
+  //Rechts haaks
+  
+
+  
+
+
+
+ 
+
+
+  //vanaf hier ff negeren
+  //--------------------------------------------------------------------------------------------------------------
+  
   //Driving forward
   //Motor A forward @ full speed
   digitalWrite(12, HIGH); //Establishes forward direction of Channel A
@@ -57,7 +183,7 @@ void loop(){
   
   
   delay(1000);
-  linksSlap();    //zie besturing.cpp voor uitleg
+  
   //Turning left or right idk
   //Motor A forward @ half speed
   digitalWrite(12, HIGH); //Establishes forward direction of Channel A
