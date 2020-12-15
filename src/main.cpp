bbe61143de808ae812cb2eeb6ffd293f3f2a6b63
tim-veldhuizen <461202@student.fontys.nl>
2020-12-15 12:04:09 +0100

    debounce aangepast

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 20;    // the debounce time; increase if the output flickers
 
 #define DS1 22
 #define DS2 24
