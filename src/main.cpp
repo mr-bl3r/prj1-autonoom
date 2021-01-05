83d3a0dbb92bbfe64b668c008196c5a04880e39f
Justin Griffioen <j.griffioen@student.fontys.nl>
2021-01-05 11:25:35 +0100

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
+#define DS1 22
+#define DS2 24
+#define DS3 26
+#define DS4 28
+#define DS5 30
+#define DS6 32
 
+int delayR = 50;
+int bl = 1;
+int wh = 0;
+
+int s1, s2, s3, s4, s5, s6;
+int ls1, ls2, ls3, ls4, ls5, ls6;
