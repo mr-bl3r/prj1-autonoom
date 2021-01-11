de2fcad9f4d305d3d54b0225b76c155e0b05a88e
Justin <joliegraap@gmail.com>
2021-01-12 00:48:35 +0100

    Co-authored-by: Veldhuizen,Tim W.E. <461202@student.fontys.nl> Co-authored-by: Latestein,Boyd B. van <455430@student.fontys.nl>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 /*
-** Line Follower Basic v. 5
+** Line Follower Basic. 5
 ** gemaakt door de boy Boyds
 */
 #include <arduino.h>
-
 
 /* Define the pins for the IR receivers */
 const int irPins[5] = {A8, A9, A10, A11, A12};
@@ -45,43 +44,16 @@ int maxSpeed = 255;
 /* variables to keep track of current speed of motors */
 int motorLSpeed = 0;
 int motorRSpeed = 0;
-int s6 = 29;
+
 void setup() {
     Serial.begin(9600);
-  
    
   /* Set-up IR sensor pins as input */
   for (int i = 0; i < 5; i++) {
     pinMode(irPins[i], INPUT);
   }
-  
-  /* Change the PWM frequency of digital pins 5 and 6 (timer0) to Phase-correct PWM of 31.250 kHz
-  from the default of ~500Hz. Using code from Adjusting PWM Frequencies
-  http://playground.arduino.cc/Main/TimerPWMCheatsheet
-  
-  This requires a separate change in the wiring.c function in the Arduino program files 
-  hardware\arduino\cores\arduino\wiring.c from:
-  #define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
-  
-  to:
-  #define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(1 * 510))
-  
-  Without the change to wiring.c time functions (millis, delay, as well as libraries using them
-  will not work corectly.
-  */
-  //TCCR0A = _BV(COM0A1) | _BV(COM0B1) | _BV(WGM01) | _BV(WGM00); 
-  //TCCR0B = _BV(CS00); 
 }
 
-void MotorLstop(){
-   digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-    digitalWrite(12, LOW); //Establishes reverse direction of Channel A
-
-}
-
-void MotorRstop(){
- 
-}
 void Scan() {
   // Initialize counters, sums etc.
  
@@ -99,16 +71,16 @@ void Scan() {
     count = count + irSensorDigital[i];
     int b = 4-i;
     irSensors = irSensors + (irSensorDigital[i]<<b);
-    } 
+    }    
 }
 
 
 void UpdateError() {
   
   errorLast = error;  
-
+    
   switch (irSensors) {
-     
+     //Serial.println(irSensors);
     case B00000:
        if (errorLast < 0) { error = -180;}
        else if (errorLast > 0) {error = 180;}
@@ -116,6 +88,7 @@ void UpdateError() {
      
      case B10000: // leftmost sensor on the line
        error = -150;
+       Serial.println("hikke tikke");
        break;
       
      case B01000:
@@ -132,76 +105,62 @@ void UpdateError() {
        
      case B00001: // right most sensor //v2
        error = 150;
+       Serial.println("hikke naar rechts");
        break;           
 
-    // case B000001: // rigtmost sensor on the line
-     //  error = 150;
-    //   break;
-       
 /* 2 Sensors on the line */         
      
      case B11000:
-       error = -120;
+       error = -150;
        break;
       
      case B01100:           // licht links
-       error = -60;
+       error = -90;
        break;
 
      case B00110:           // licht rechts
-       error = 60;
+       error = 90;
        break;
 
      case B00011: 
-       error = 120;
+       error = 150;
        break;           
      
-     case B10100:           //scherp links
-        error = -150;
+     case B10100:           //scherp links 
+        error = 0;
         break; 
  
      case B00101:           // scherp rechts
-       error = 150;
-       break; 
-// 3 Sensors on the line     
+       error = 0;
+       break;
+/* 3 Sensors on the line */    
        // Als de auto op 2, 1 en 3 staat probeer case B01110
        
      case B11100:
-     //case B01110:
        error = -150;
+       Serial.println("i should be going left"); // added this
        break;
       
-     //case B00111:
+     
      case B00111:
        error = 150;
-
+       Serial.println("i should be going right"); // added this
        break;
 
- // 4 Sensors on the line       
+ /* 4 Sensors on the line */      
      case B11110:           //links af maar met 1 foute lezing error
-       error = -150;
+       error = -150;        //mogelijk pauze symbool?
        break;
           
      case B01111:           //rechts af maar met 1 foute lezing error
-       error = 150;
+       error = 150;         //mogelijk pauze symbool?
        break;
                  
-// 5 Sensors on the line      
+/* 5 Sensors on the line */      
 
      case B11111:
-       //interupt = 1; // increment interupts when pause stop line is found
-       error = 0;
-
-       
-       // tijdelijk test voor het stoppen van de motoren
-       //motor stop R 
-      digitalWrite(9, LOW);   
-      digitalWrite(12, HIGH); 
-      // motor L stop
-      digitalWrite(8, LOW);   
-      digitalWrite(13, HIGH); 
-
-       delay(4000);
+       interupt = 1; // increment interupts when pause stop line is found
+       error = 0;     //pauze en ook stop
        break;
    
      default:
@@ -232,12 +191,8 @@ void UpdateCorrection() {
   } 
   
   else if (error >=150 && error < 180) {
-    //hij moet stil staan en draaien in zijn hart
-    while(s6 == 0){
-      digitalRead(s6);
-    }  
-    //motors aanpassen
-    correction = 255;
+    correction = 305;
+    
   }   
 
   else if (error >=180) {
@@ -258,7 +213,6 @@ void UpdateCorrection() {
   
   else if (error <= -90 && error > -120) {
     correction = -55;
-
   }  
   
   else if (error <= -120 && error > -150) {
@@ -266,12 +220,8 @@ void UpdateCorrection() {
   } 
   
   else if (error <= -150 && error > -180) {
-    //hij moet stil staan en draaien in zijn hart
-    while(s6 == 0){
-      digitalRead(s6);
-    }  
-    //motors aanpassen
-    correction = 255;
+    correction = -305;
+    
   }   
 
   else if (error <= -180) {
@@ -279,13 +229,13 @@ void UpdateCorrection() {
   }
   
   if (correction > 0) {
-    motorRSpeed = maxSpeed - correction;          //bvb 255 - 40 = 215 pwm
+    motorRSpeed = maxSpeed - correction;
     motorLSpeed = maxSpeed;
   }
   
   else if (correction < 0) {
     motorRSpeed = maxSpeed;
-    motorLSpeed = maxSpeed + correction;          //bvb 255 +- 40 = 215 pwm
+    motorLSpeed = maxSpeed + correction;
   }
 else if (correction == 0) {
     motorRSpeed = maxSpeed;
@@ -294,9 +244,6 @@ else if (correction == 0) {
 }
 
 void Drive() {
-  Serial.println(motorLSpeed);
-  Serial.print(motorRSpeed);
-  Serial.println(" ");
   if (motorRSpeed > 255) {motorRSpeed = 255;}
   else if (motorRSpeed < -255) {motorRSpeed = -255;}
   
@@ -315,7 +262,7 @@ void Drive() {
     analogWrite(3, motorRSpeed);
   } 
   
-  else if (motorRSpeed == 0) { // right motor fast stop
+  else if (motorRSpeed == 0 || motorLSpeed == 0) { // right motor fast stop
      digitalWrite(9, HIGH);   //Disengage the Brake for Channel A
     digitalWrite(12, LOW); //Establishes reverse direction of Channel A
     analogWrite(3, 0);
@@ -324,17 +271,21 @@ void Drive() {
   if (motorLSpeed >= 0) { // left motor forward (using PWM)
      digitalWrite(8, LOW);   //Disengage the Brake for Channel A
     digitalWrite(13, HIGH); //Establishes fwd direction of Channel A
-    analogWrite(11, motorRSpeed);
+    analogWrite(11, motorLSpeed);
   } 
   
   else if (motorLSpeed < 0) { // left motor reverse (using PWM)
      digitalWrite(8, LOW);   //Disengage the Brake for Channel A
     digitalWrite(13, LOW); //Establishes reverse direction of Channel A
-    analogWrite(11, motorRSpeed);
+    analogWrite(11, motorLSpeed);
   } 
 
 }
+
+
 void loop() {
+ 
+
  Scan();
  UpdateError();
  UpdateCorrection();
